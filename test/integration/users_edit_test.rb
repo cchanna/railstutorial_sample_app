8707c5b7c0ebf5ec1f3cebef4421a4d14df055cc
Cerisa <cerisa@strangerelics.com>
2015-05-25 12:30:02 -0700

    9.2 - Authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -6,6 +6,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	patch user_path(@user), user: { name: "",
@@ -16,6 +17,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+  	log_in_as(@user)
   	get edit_user_path(@user)
   	assert_template 'users/edit'
   	name = "Foo Bar"
@@ -27,6 +29,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   	assert_not flash.empty?
   	assert_redirected_to @user
   	@user.reload
+  	assert_equal name, @user.name
+  	assert_equal email, @user.email
+  end
+
+  test "succesful edit with friendly forwarding" do
+  	get edit_user_path(@user)
+  	log_in_as(@user)
+  	assert_redirected_to edit_user_path(@user)
+  	name = "Foo Bar"
+  	email = "foo@bar.com"
+  	patch user_path(@user), user: { name: name,
+  																 email: email,
+  															password: "",
+  								 password_confirmation: "" }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
   	assert_equal name, @user.name
   	assert_equal email, @user.email
   end
