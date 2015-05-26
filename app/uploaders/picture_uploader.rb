84fb881e43b9279c06e13c0c185064e28286f87f
Cerisa <cerisa@strangerelics.com>
2015-05-25 20:23:03 -0700

    11.4.3 - Image resizing

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -4,7 +4,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -26,7 +27,8 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  #
+  
+
   # def scale(width, height)
   #   # do something
   # end
